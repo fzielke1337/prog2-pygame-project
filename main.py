5784c889ffe6b62f227be21ef292f78882cdf959
Johanna Wieber <johanna.wieber@stu.haw-kiel.de>
2026-03-27 16:48:16 +0100

    Spieler bewegt sich nur ein Feld pro Drücken

--- main.py
+++ main.py
@@ -20,35 +20,46 @@ collision_active = False
 
 running = True
 while running:
+    old_x = player.x
+    old_y = player.y
+    moved = False
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # Bewegungscode basierend auf Erklärung durch ChatGPT (verstanden und angepasst)
-    keys = pygame.key.get_pressed()
+    # Bewegung nur ein Feld pro Drücken // Mit der Hilfe von ChatGPT, verstanden und angepasst
+    # vorher get_pressed() = dauerhafte Bewegung, jetzt KEYDOWN = einmal pro Taste drücken
+        if event.type == pygame.KEYDOWN:
+            old_x = player.x
+            old_y = player.y
+        
 
-    old_x = player.x
-    old_y = player.y
-
-    if keys[pygame.K_LEFT]:
-        player.x -= 5
-    if keys[pygame.K_RIGHT]:
-        player.x += 5
-    if keys[pygame.K_UP]:
-        player.y -= 5
-    if keys[pygame.K_DOWN]:
-        player.y += 5
+            if event.key == pygame.K_LEFT:
+                player.x -= 60
+                moved = True
+            elif event.key == pygame.K_RIGHT:
+                player.x += 60
+                moved = True
+            elif event.key == pygame.K_UP:
+                player.y -= 60
+                moved = True
+            elif event.key == pygame.K_DOWN:
+                player.y += 60
+                moved = True
 
     # Nur 1 Mal Crash printen, kein Durchlaufen durch die Wand
-    collision = False
-    for wall in walls:
-        if player.colliderect(wall):
-            collision = True
-            if not collision_active:
-                print("Crash")
-                collision_active = True
-            player.x = old_x
-            player.y = old_y
+    if moved == True:
+        collision = False
+        for wall in walls:
+            if player.colliderect(wall):
+                collision = True
+                if not collision_active:
+                    print("Crash")
+                    collision_active = True
+                player.x = old_x
+                player.y = old_y
+                break
    
 
     # Spieler darstellen
