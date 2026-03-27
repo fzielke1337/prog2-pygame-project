78831b1d6881808d161d8ce90b5a03d48aef60f1
Johanna Wieber <johanna.wieber@stu.haw-kiel.de>
2026-03-27 14:53:56 +0100

    Settings erstellt und Screen Höhe und Tiefe hingeschoben

--- level.py
+++ level.py
@@ -1,10 +1,12 @@
 import pygame
+from settings import WIDTH, HEIGHT
+
 
 # Wände erstellt durch Ausprobieren
-wall_top = pygame.Rect(0, 0, 800, 10)
-wall_bottom = pygame.Rect(0, 590, 800, 10)
-wall_left = pygame.Rect(0, 0, 10, 600)
-wall_right = pygame.Rect(790, 0, 10, 600)
+wall_top = pygame.Rect(0, 0, WIDTH, 10)
+wall_bottom = pygame.Rect(0, 590, WIDTH, 10)
+wall_left = pygame.Rect(0, 0, 10, HEIGHT)
+wall_right = pygame.Rect(790, 0, 10, HEIGHT)
 
 # Alle Wände in Liste
 walls = [wall_top, wall_bottom, wall_left, wall_right]
